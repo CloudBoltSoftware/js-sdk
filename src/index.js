402075525a6283b65fcd4a0313127a6c95b4e9c9
Eric Hussmann <87334855+ehussm@users.noreply.github.com>
2022-04-27 12:19:04 -0700

    Revert "Feature/DEV-21609-dashboard-admin"

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import { baseApi, clearAuthHeader, setAuthHeader } from './api/baseApi'
 import crud, { setErrorHandler } from './api/crudOperations'
 import ApiTokenService from './api/services/v3/cmp/ApiTokenService'
 import EulaService from './api/services/v3/cmp/EulaService'
-import GroupsService from './api/services/v3/cmp/GroupsService'
 import LicensingService from './api/services/v3/cmp/LicensingService'
 import ProductInfoService from './api/services/v3/cmp/ProductInfoService'
 import UiExtensionComponentsService from './api/services/v3/cmp/UiExtensionComponentsService'
@@ -22,7 +21,6 @@ export const createApi = () => {
       cmp: {
         apiToken: ApiTokenService,
         eula: EulaService,
-        groups: GroupsService,
         licensing: LicensingService,
         productInfo: ProductInfoService,
         uiExtensions: UiExtensionComponentsService,
