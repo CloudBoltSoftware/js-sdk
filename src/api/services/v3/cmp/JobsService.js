2970955548de6979edbe8a8cfaa0bb6afa5121b4
jjoshi-cb <jjoshi@cloudbolt.io>
2022-05-23 23:59:29 +0530

    Updated: 1. Included endpoint getting job via job-id 2. Removed the options containing filters, page_size

--- src/api/services/v3/cmp/JobsService.js
+++ src/api/services/v3/cmp/JobsService.js
@@ -8,10 +8,13 @@ export default {
    * @param options anything parsable by URLSearchParams. See useful options here https://docs.cloudbolt.io/articles/#!cloudbolt-latest-docs/api-conventions/a/h2__904191799
    * @returns
    */
-  list: (options) =>
-    crud.getItems(URL, {
-      filter: 'type.iexact:functionaltest',
-      page_size: 100,
-      ...options
-    })
+  list: (options) => crud.getItems(URL, options),
+
+  /**
+   * Retrieve an existing job
+   * @param {string} id or global_id
+   * @param options anything parsable by URLSearchParams. See useful options here https://docs.cloudbolt.io/articles/#!cloudbolt-latest-docs/api-conventions/a/h2__904191799
+   * @returns {Promise} resolves with a cloudbolt API Response object of Job objects
+   */
+  get: (id, options) => crud.getItemById(URL, id, options)
 }
--- src/api/services/v3/cmp/JobsService.test.js
+++ src/api/services/v3/cmp/JobsService.test.js
@@ -6,7 +6,13 @@ test('list calls the correct endpoint', async () => {
     data: [{ hello: 'world' }]
   })
   await JobsService.list()
-  expect(mockFn).toHaveBeenCalledWith(
-    '/v3/cmp/jobs/?filter=type.iexact%3Afunctionaltest&page_size=100'
-  )
+  expect(mockFn).toHaveBeenCalledWith('/v3/cmp/jobs/')
+})
+
+test('get calls the correct endpoint', async () => {
+  const mockFn = jest.spyOn(baseApi, 'get').mockResolvedValue({
+    data: { hello: 'world' }
+  })
+  await JobsService.get('job-id')
+  expect(mockFn).toHaveBeenCalledWith('/v3/cmp/jobs/job-id/')
 })
