f47db4cf31a5c5160d704061851c9c13e4f301a7
bhadresa <pbhadresa@cloubolt.io>
2022-06-09 14:33:51 +0530

    Update: api for fetching more resource records

--- src/api/services/v3/cmp/ResourceService.js
+++ src/api/services/v3/cmp/ResourceService.js
@@ -2,5 +2,5 @@ import crud from "../../../crudOperations"
 
 export default {
     getResourceTypes: async () => await crud.getItems('v3/cmp/resourceTypes'),
-    getResources: async () => await crud.getItemByEndpoint('v3/cmp/resources/')
+    getResources: async () => await crud.getItemByEndpoint('v3/cmp/resources/', {page_size: 100})
 }
