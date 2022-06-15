099427b1f2b1699966c81cb10793beabdd6b5f30
bhadresa <pbhadresa@cloubolt.io>
2022-06-15 13:31:37 +0530

    Update: added API for fetching server summary

--- src/api/services/v3/cmp/ResourceService.js
+++ src/api/services/v3/cmp/ResourceService.js
@@ -2,5 +2,6 @@ import crud from "../../../crudOperations"
 
 export default {
     getResourceTypes: async () => await crud.getItemByEndpoint('v3/cmp/resourceTypes', {page_size: 100}),
-    getResources: async () => await crud.getItemByEndpoint('v3/cmp/resources', {page_size: 100})
+    getResources: async () => await crud.getItemByEndpoint('v3/cmp/resources', {page_size: 100}),
+    getServerSummary: async () => await crud.getItemByEndpoint('v3/cmp/serverSummary', {page_size: 100})
 }
