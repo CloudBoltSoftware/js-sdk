3d76431f30bcf33a929d1bd830f68351631a59e4
nisarga-cb <nathavale@cloudbolt.io>
2022-06-29 21:40:26 +0530

    fixed index.js files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import { baseApi, clearAuthHeader, setAuthHeader } from './api/baseApi'
 import crud, { setErrorHandler } from './api/crudOperations'
-import ApiTokenService from './api/services/v3/cmp/ApiTokenService'
-import EulaService from './api/services/v3/cmp/EulaService'
-import GroupsService from './api/services/v3/cmp/GroupsService'
-import LicensingService from './api/services/v3/cmp/LicensingService'
-import ProductInfoService from './api/services/v3/cmp/ProductInfoService'
-import SystemService from './api/services/v3/cmp/SystemService'
-import UiExtensionComponentsService from './api/services/v3/cmp/UiExtensionComponentsService'
-import UserService from './api/services/v3/cmp/UserService'
-import DashboardService from './api/services/v3/DashboardService'
 import v3 from './api/services/v3'
 
 /**
@@ -26,19 +17,6 @@ export const createApi = ({ errorHandler } = {}) => {
       clearAuthHeader,
       setErrorHandler
     },
-    v3: {
-      cmp: {
-        apiToken: ApiTokenService,
-        eula: EulaService,
-        groups: GroupsService,
-        licensing: LicensingService,
-        productInfo: ProductInfoService,
-        uiExtensions: UiExtensionComponentsService,
-        users: UserService,
-        system: SystemService
-      },
-      dashboard: DashboardService
-    }
     v3
   }
 }
