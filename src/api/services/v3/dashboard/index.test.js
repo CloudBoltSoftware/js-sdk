225c8ae5e40a9be305ff85f081d2ad2de7d4f860
Orvis Evans <oevans@cloudbolt.io>
2022-07-01 11:53:13 -0700

    ENG-22155 Add index test

--- src/api/services/v3/dashboard/index.test.js
+++ src/api/services/v3/dashboard/index.test.js
@@ -4,10 +4,13 @@ it('should export an object', () => {
   expect(typeof dashboard).toBe('object')
 })
 
-test.each(['widgets'])('should export a %s service', (serviceName) => {
-  expect(dashboard[serviceName]).toBeDefined()
-  expect(typeof dashboard[serviceName]).toBe('object')
-})
+test.each(['announcements', 'widgets'])(
+  'should export a %s service',
+  (serviceName) => {
+    expect(dashboard[serviceName]).toBeDefined()
+    expect(typeof dashboard[serviceName]).toBe('object')
+  }
+)
 
 test.each([
   'getBlueprints',
