b648c61f61d0f38c313ad74542701f2756591e06
jjoshi-cb <jjoshi@cloudbolt.io>
2023-01-03 16:19:33 +0530

    Added: 1. Get call for api/v3/cmp/servers/__serverId__

--- src/api/services/v3/cmp/ServerService.js
+++ src/api/services/v3/cmp/ServerService.js
@@ -9,4 +9,12 @@ export default {
    * @returns {Promise} resolves with a paginated list of existing Resource Types
    */
   list: (options) => crud.getItems(URL, options),
+
+  /**
+   * Retrieve an existing Server by id
+   * @param {string} id or global_id
+   * @param {Object} options anything parsable by URLSearchParams. See useful options here https://docs.cloudbolt.io/articles/#!cloudbolt-latest-docs/api-conventions/a/h2__904191799
+   * @returns {Promise} resolves with a cloudbolt API Response object of the Server object
+   */
+  get: (id, options) => crud.getItemById(URL, id, options)
 }
