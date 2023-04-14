a96961cdde839ca447810c94d4c9487fe98c3cca
Eric Hussmann <ehussmann@cloudbolt.io>
2023-04-14 12:47:32 -0700

    Add job actions endpoints to jobs service [ENG-24390]

--- src/api/services/v3/cmp/JobsService.test.js
+++ src/api/services/v3/cmp/JobsService.test.js
@@ -16,3 +16,29 @@ test('get calls the correct endpoint', async () => {
   await JobsService.get('job-id')
   expect(mockFn).toHaveBeenCalledWith('/v3/cmp/jobs/job-id/')
 })
+
+test('cancel calls the correct endpoint', async () => {
+  const mockFn = jest.spyOn(baseApi, 'post').mockResolvedValue()
+
+  const payload = {
+    jobs: ['/v3/cmp/jobs/job-id/']
+  }
+  await JobsService.cancel(payload)
+  expect(mockFn).toHaveBeenCalledWith('/v3/cmp/jobs/', payload)
+})
+
+test('clone calls the correct endpoint', async () => {
+  const mockFn = jest.spyOn(baseApi, 'post').mockResolvedValue()
+
+  await JobsService.clone('job-id')
+  expect(mockFn).toHaveBeenCalledWith('/v3/cmp/jobs/job-id/clone/', null)
+})
+
+test('resume calls the correct endpoint', async () => {
+  const mockFn = jest.spyOn(baseApi, 'patch').mockResolvedValue()
+  const payload = {
+    resume_type: 'STEP'
+  }
+  await JobsService.resume('job-id', payload)
+  expect(mockFn).toHaveBeenCalledWith('/v3/cmp/jobs/job-id/resume/', payload)
+})
