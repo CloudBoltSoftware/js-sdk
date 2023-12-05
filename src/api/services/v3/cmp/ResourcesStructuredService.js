5aa268a8008e0f24b0c30c4844928d60d0ee97ad
nisarga-cb <nathavale@cloudbolt.io>
2023-12-05 18:04:11 +0530

    CMP-1053 APIs for getting,adding,editing,deleting parameters for resources

--- src/api/services/v3/cmp/ResourcesStructuredService.js
+++ src/api/services/v3/cmp/ResourcesStructuredService.js
@@ -16,5 +16,50 @@ export default {
    * @param {Object} options anything parsable by URLSearchParams. See useful options here https://docs.cloudbolt.io/articles/#!cloudbolt-latest-docs/api-conventions/a/h2__904191799
    * @returns {Promise} resolves with a cloudbolt API Response object of the Structured Resource object
    */
-  get: (id, options) => crud.getItemById(URL, id, options)
+  get: (id, options) => crud.getItemById(URL, id, options),
+
+  /**
+   * Retrieves parameter values for a resource
+   * @param {string} id id of the resource
+   * @returns {Promise} resolves with a cloudbolt API Response object of the Structured Resource object
+   */
+
+  getParameters: (id) =>
+    crud.getItemById(`api/v3/cmp/resourcesStructured/${id}/parameterValues`),
+
+  /**
+   * Create a parameter for a resource
+   * @param {string} id id of the resource
+   * @param {Object} options Additional options for customization
+   * @returns {Promise} resolves with a cloudbolt API Response object of the Structured Resource object
+   */
+  createParameter: (id, options) =>
+    crud.createNewItem(
+      `api/v3/cmp/resourcesStructured/${id}/parameterValues`,
+      options
+    ),
+
+  /**
+   * Edit a parameter for a resource
+   * @param {string} id id of the resource
+   * @param {Object} options Additional options for customization
+   * @param {string} options.customFieldId - The ID of a custom field parameter
+   * @returns {Promise} resolves with a cloudbolt API Response object of the Structured Resource object
+   */
+  editParameter: (id, options) =>
+    crud.patchItemById(
+      `api/v3/cmp/resourcesStructured/${id}/parameterValues/${options.customFieldId}`,
+      options
+    ),
+
+  /**
+   * Delete a parameter for a resource
+   * @param {string} id id of the resource
+   * @param {string} customFieldId - The ID of a custom field parameter
+   * @returns {Promise} resolves with a cloudbolt API Response object of the Structured Resource object
+   */
+  deleteParameter: (id, customFieldId) =>
+    crud.deleteItemById(
+      `api/v3/cmp/resourcesStructured/${id}/parameterValues/${customFieldId}`
+    )
 }
--- src/api/services/v3/cmp/ResourcesStructuredService.test.js
+++ src/api/services/v3/cmp/ResourcesStructuredService.test.js
@@ -20,3 +20,30 @@ test('get calls the correct endpoint', async () => {
     `${RESOURCES_STRUCTURED_ENDPOINT}resource-id/`
   )
 })
+
+test('get parameter values calls the correct endpoint', async () => {
+  const mockFn = jest.spyOn(baseApi, 'get').mockResolvedValue({
+    data: { hello: 'world' }
+  })
+  await ResourcesStructuredService.getParameters('resource-id')
+  expect(mockFn).toHaveBeenCalledWith(
+    '/api/v3/cmp/resourcesStructured/resource-id/parameterValues/'
+  )
+})
+
+test('create parameter values calls the correct endpoint', async () => {
+  const mockFn = jest.spyOn(baseApi, 'post').mockResolvedValue({
+    data: { hello: 'world' }
+  })
+
+  const options = {
+    fieldId: 'CF-7m1gvvpg',
+    value: 'deletion'
+  }
+
+  await ResourcesStructuredService.createParameter('resource-id', options)
+  expect(mockFn).toHaveBeenCalledWith(
+    '/api/v3/cmp/resourcesStructured/resource-id/parameterValues/',
+    options
+  )
+})
